4f92f86221d3c89a0cabcefb64659cac1ccc4b85
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-27 02:14:25 +0000

    more advanced error handler

--- qr.rb
+++ qr.rb
@@ -6,15 +6,15 @@ puts "\nLet's make a QR code! What kind of code would you like to create? (enter
   3. Send a text message\n
   Enter 4 to exit\n\n"
 
-user_selection = gets.chomp.to_i
+user_selection = gets.chomp
 
-while user_selection > 4
+while user_selection.to_i > 4 or user_selection.to_i.class != "Integer"
   puts "\n\"#{user_selection}\" is an invalid selection. Please enter a number from 1-4. See QR code options below:\n
     1. Open a URL
     2. Join a wifi network
     3. Send a text message\n
     Enter 4 to exit\n\n"
-  user_selection = gets.chomp.to_i
+  user_selection = gets.chomp
 end
 
 =begin
