a270432de8590729223213cd818aed6754b2d417
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-27 02:10:45 +0000

    basic error handler complete

--- qr.rb
+++ qr.rb
@@ -1,5 +1,25 @@
 require "rqrcode"
 
+puts "\nLet's make a QR code! What kind of code would you like to create? (enter 1-4)\n
+  1. Open a URL
+  2. Join a wifi network
+  3. Send a text message\n
+  Enter 4 to exit\n\n"
+
+user_selection = gets.chomp.to_i
+
+while user_selection > 4
+  puts "\n\"#{user_selection}\" is an invalid selection. Please enter a number from 1-4. See QR code options below:\n
+    1. Open a URL
+    2. Join a wifi network
+    3. Send a text message\n
+    Enter 4 to exit\n\n"
+  user_selection = gets.chomp.to_i
+end
+
+=begin
+gets.chomp
+
 # use the RQRCode::QRCode class to encode some text
 qrcode = RQRCode::QRCode.new("https://en.wikipedia.org/wiki/QR_code")
 
@@ -8,3 +28,4 @@ png = qrcode.as_png({:size => 500})
 
 # write the image data to a file
 IO.binwrite("sometext.png", png.to_s)
+=end
