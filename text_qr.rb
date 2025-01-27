96cedd8ee376ec0ef9bceae18ae6ccfdbfd60fb0
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-27 02:01:36 +0000

    textmessage_qr_done

---
+++ text_qr.rb
@@ -0,0 +1,10 @@
+require "rqrcode"
+
+# use the RQRCode::QRCode class to encode some text
+qrcode = RQRCode::QRCode.new("SMSTO:9726797820:Hi Alice! It's")
+
+# use the .as_png method to create a 500px by 500px image
+png = qrcode.as_png({:size => 500})
+
+# write the image data to a file
+IO.binwrite("sometext.png", png.to_s)
