40b838fcbbed2b8c86c54cf3f12742ca546561bb
AakashB-Sky <62520487+AakashB-Sky@users.noreply.github.com>
2025-01-27 02:27:08 +0000

    starting to write qr code code

--- qr.rb
+++ qr.rb
@@ -1,13 +1,16 @@
 require "rqrcode"
 
+# ask user for 1-4 input
+
 puts "\nLet's make a QR code! What kind of code would you like to create? (enter 1-4)\n
   1. Open a URL
   2. Join a wifi network
   3. Send a text message\n
   Enter 4 to exit\n\n"
 
-user_selection = gets.chomp
+  user_selection = gets.chomp
 
+# check for valid 1-4 input
 while user_selection.to_i > 4 or user_selection.to_i == 0
   puts "\n\"#{user_selection}\" is an invalid selection. Please enter a number from 1-4. See QR code options below:\n
     1. Open a URL
@@ -15,6 +18,27 @@ while user_selection.to_i > 4 or user_selection.to_i == 0
     3. Send a text message\n
     Enter 4 to exit\n\n"
   user_selection = gets.chomp
+end
+
+# save user input as integer
+user_selection = user_selection.to_i
+
+# create QR code based on selection
+
+100.times do |counter|
+  print "–"
+end
+print "\n"
+
+if user_selection == 1
+  puts "\nLet's make a QR code that links to a URL!\n\n"
+  
+elsif user_selection == 2
+  puts "\nLet's make a QR code that joins a wifi network!\n\n"
+elsif user_selection == 3
+  puts "\nLet's make a QR code that sends a text message!\n\n"
+elsif user_selection ==4
+  puts "\nLooks like you don't need a QR code. Come back if you do!\n\n"
 end
 
 =begin
